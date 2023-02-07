9e42c7b00235de20be0ab18930167c25b4ecef48
sufiYAN-87 <116790492+sufiYAN-87@users.noreply.github.com>
2023-02-08 02:40:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// javascript code goes here
+let disp = document.getElementById("blank");
+async function getRandomUser(){
+    const response = await fetch('https://randomuser.me/api/'); 
+    const data = await response.json();
+    const user = data.results[0];
+    display(user);
 
+}
+function display(user){
+    let name = document.getElementById("name")
+    name.innerText = `${user.name.first + " " + user.name.last}`
 
+    let img = document.getElementById("img");
+    img.setAttribute('src', `${user.picture.large}`)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+    let age = document.getElementById("age");
+    age.addEventListener('click' , () =>{
+        disp.innerHTML= `${user.dob.age}`
+    })
+
+    let email = document.getElementById("email");
+    email.addEventListener('click' , () =>{
+        disp.innerHTML= `${user.email}`
+    })
+
+    let phone = document.getElementById("phone");
+    phone.addEventListener('click' , () =>{
+        disp.innerHTML= `${user.phone}`
+    })
+}
+        getRandomUser();
+        let newu = document.getElementById("getUser");
+    newu.addEventListener('click' , () =>{
+        getRandomUser()
+        disp.innerText = "";
+    })
